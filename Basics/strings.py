84cf21e2b7419c695ba23d57f7581c2295678782
Derrick Mwangi <ngarariderrick11@gmail.com>
2026-03-06 11:42:50 +0300

    commit 003

--- Basics/strings.py
+++ Basics/strings.py
@@ -4,4 +4,27 @@ animal = "Octopus"
 quote = "It is a beautiful day!"
 another_quote = 'Octo says "Hello!"'
 
-print(quote[8:17])
+#print(quote[8:17])
+# print(name[::1])
+
+# layout = "*" + "-" * 20 + "*"
+# print(layout)
+
+# first_name = "Derrick"
+# last_name = "Mwangi"
+
+# print(len(first_name))
+# print(first_name.capitalize())
+# print(first_name.swapcase())
+
+#--------------Stripping whitespace-----------------
+language = "     Python     "
+print(language.rstrip())
+print(language.lstrip())
+print(language.strip())
+
+#-----------------replacing strings-----------------
+hello = "Hello, world!"
+print(hello.replace("world", "Python"))
+
+print(hello.count("o"))
